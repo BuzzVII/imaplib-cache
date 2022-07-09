4838ef93b1829709898ea0bdff04948ad4bfa29f
Kristian Weegink <k.weegink@gmail.com>
2022-07-09 23:11:09 +1000

    Package setup
    Set up the appropriate directories and files for a python package.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='imaplib-cache',
+      version='0.0',
+      description='A persistent cache for IMAP fetch',
+      author='Kristian Weegink',
+      author_email='k.weegink@gmail.com',
+      url='',
+      packages=['imaplib-cache'],
+     )
